730354312476770e1d8d067289ffa0e1238b8028
afyaa <afyaaenaya@gmail.com>
2024-08-11 14:11:11 +0300

    Fixed an error with get_output_file

--- get_directories.py
+++ get_directories.py
@@ -19,20 +19,22 @@ def get_output_file():
     
     global output_file_path
 
-    if not output_file_path:
-        output_file_path = input("Enter the path to the .csv file for the results. If the file does not exist, it will be created:")
-        
-        while not output_file_path.endswith(".csv"):
-            output_file_path = input("The file has to be a .csv file. Enter the path to the .csv file for the results. If the file does not exist, it will be created:")
-        
-        if not os.path.exists(output_file_path):
-            create_confirmation = input(f"Do you want to create a new file at {output_file_path}? [y/n]")
-            
-            while create_confirmation not in ["y", "n"]:
-                create_confirmation = input(f"Invalid input. Do you want to create a new file at {output_file_path}? [y/n]")
+    if output_file_path:
+        return output_file_path
 
-            if create_confirmation == "n":
-                output_file_path = ''
-                get_output_file()
+    output_file_path = input("Enter the path to the .csv file for the results. If the file does not exist, it will be created:")
+
+    while not output_file_path.endswith(".csv"):
+        output_file_path = input("The file has to be a .csv file. Enter the path to the .csv file for the results. If the file does not exist, it will be created:")
+
+    if not os.path.exists(output_file_path):
+        create_confirmation = input(f"Do you want to create a new file at {output_file_path}? [y/n] ")
+
+        while create_confirmation not in ["y", "n"]:
+            create_confirmation = input(f"Invalid input. Do you want to create a new file at {output_file_path}? [y/n] ")
+
+        if create_confirmation == "n":
+            output_file_path = None
+            return get_output_file()
 
     return output_file_path
