586d2c77b97578fed5887c23d81ce742f01f8e81
afyaa <afyaaenaya@gmail.com>
2024-08-18 14:28:48 +0300

    added log functionality

---
+++ log.py
@@ -0,0 +1,16 @@
+import os
+
+def get_log():
+    log_path = os.path.join(os.getcwd(), 'log.txt')
+    if not os.path.exists(log_path):
+        with open(log_path, 'w') as log:
+            pass # creates an empty log.txt file
+        print(f'Created log file at {log}')
+
+    with open(log_path, 'r') as log:
+        return set(line.strip() for line in file)
+
+def log_processed_file(file_path):
+    log_path = os.path.join(os.getcwd(), 'log.txt')
+    with open(log_path, 'a') as log:
+        log.write(f'{file_path}\n')
